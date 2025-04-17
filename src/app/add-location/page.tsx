a15cb1e71a0ddd93371fd9b3071911e66b9c2819
Caner Yayla <caneryaylaa@icloud.com>
2025-04-17 11:47:59 +0300

    file structure created

---
+++ src/app/add-location/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddLocationPage = () => {
+  return <div>AddLocationPage</div>;
+};
+
+export default AddLocationPage;
---
+++ src/app/draw-route/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DrawRoutePage = () => {
+  return <div>DrawRoutePage</div>;
+};
+
+export default DrawRoutePage;
